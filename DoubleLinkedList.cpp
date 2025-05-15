b1ff5d603d317a45ecc1f7858f115def145ed986
DzakiAhmadFauzi <dzakfauzi229@gmail.com>
2025-05-15 21:04:37 +0700

    commit 2

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -15,3 +15,70 @@ class DoubleLinkedList
 private:
     Node *START;
 
+public:
+    DoubleLinkedList()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+
+        // step 1 : allocate memory for new node
+        Node *newNode = new Node();
+
+        // step 2 : assign value to the data fields
+        newNode->noMhs = nim;
+
+        // step 3 : insert ar the beginning if list is empty or nim is smaller than the first node
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplicate number not allowed" << endl;
+                return;
+            }
+            // step 4 : newnode.next = start
+            newNode->next = START;
+
+            // step 5 : start.prev = newnode (if start exists)
+            if (START != NULL)
+                START->prev = newNode;
+
+            // step 6 : newnode.prev = null
+            newNode->prev = NULL;
+
+            // step 7 : start = newnode
+            START = newNode;
+            return;
+        }
+
+        //  insert in between node
+        // step 8 : Locate position to insertion
+        Node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll number not allowed" << endl;
+            return;
+        }
+
+        // step 9: Insert between current and current->next
+        newNode->next = current->next; // step 9a: newnode.next = current->next
+        newNode->prev = current;       // step 9b: newnode.prev = current
+
+        // insert lost node
+        if (current->next != NULL)
+            current->next->prev = newNode; // step 9c: current->next.prev = newnode
+
+        current->next = newNode; // step 9d: current.next = newnode
+    }
+
+    
